cab9e5881a920d745b0184ecd56566320c379db1
Yıldıray Gemük <yildiray.gemuk@polynomtech.com>
2022-03-13 01:45:17 +0300

    Discount microservice

---
+++ FreeCourse.Services.Discount/Controllers/DiscountController.cs
@@ -0,0 +1,57 @@
+﻿using FreeCourse.Services.Discount.Services;
+using FreeCourse.Shared;
+using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : CustomBaseController
+    {
+        private readonly IDiscountService _discountService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public DiscountController(IDiscountService discountService, ISharedIdentityService sharedIdentityService)
+        {
+            _discountService = discountService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return CreateActionResultInstance(await _discountService.GetAll());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return CreateActionResultInstance(await _discountService.GetById(id));
+        }
+        [HttpGet("/api/[controller]/[action]/{code}")]
+        public async Task<IActionResult> GetByCode(string code)
+        {
+            var userId = _sharedIdentityService.GetUserId;
+            return CreateActionResultInstance(await _discountService.GetByCodeAndUserId(code,userId));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(Models.Discount discount)
+        {
+            return CreateActionResultInstance(await _discountService.Save(discount));
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(Models.Discount discount)
+        {
+            return CreateActionResultInstance(await _discountService.Update(discount));
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return CreateActionResultInstance(await _discountService.Delete(id));
+        }
+    }
+}
--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -17,6 +17,7 @@ namespace FreeCourse.IdentityServer
                        new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
                        new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
                        new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
+                       new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
                        new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
                    };
         public static IEnumerable<IdentityResource> IdentityResources =>
@@ -34,6 +35,7 @@ namespace FreeCourse.IdentityServer
                 new ApiScope("catalog_fullpermission","Catalog API için full erişim."),
                 new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
                 new ApiScope("basket_fullpermission","Basket API için full erişim"),
+                new ApiScope("discountfullpermission","Discount API için full erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -57,6 +59,7 @@ namespace FreeCourse.IdentityServer
                    AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
                    AllowedScopes={
                        "basket_fullpermission",
+                       "discount_fullpermission",
                        IdentityServerConstants.StandardScopes.Email,
                        IdentityServerConstants.StandardScopes.OpenId,
                        IdentityServerConstants.StandardScopes.Profile,
